99eb8ab4247205bf61ea051bfbf0e823ad754045
unknown <arya.wadhwani@gmail.com>
2021-08-22 00:08:21 +0530

    Reading Data

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,14 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// Basic CRUD Operations
+
+const {MongoClient,ObjectID} = require('mongodb')
 
 const connectionURl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+// const id = new ObjectID()
+// console.log(id.id.length)
+// console.log(id.toHexString.length)
+
 
 MongoClient.connect(connectionURl, { useNewUrlParser : true }, (error,client)=>{
     if(error){
@@ -10,49 +16,22 @@ MongoClient.connect(connectionURl, { useNewUrlParser : true }, (error,client)=>{
     }
 
     const db = client.db(databaseName)
-    
-    // db.collection('users').insertOne({
-    //     name:"Arya",
-    //     age: 19
-    // }, (error,result)=>{
+    // db.collection('users').findOne({_id:new ObjectID("61211b0026c9313204c156dc")},(error,user)=>{
     //     if(error){
-    //         return console.log("Unable to insert user")
+    //         return console.log("Unable to fetch the data")
     //     }
-    //     console.log(result.ops)
+    //     console.log(user)
     // })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name:"Aspartan",
-    //         age:22
-    //     },
-    //     {
-    //         name:"Troy",
-    //         age:26
-    //     }
-    // ],(error,result)=>{
-    //     if(error){
-    //         return console.log("Unable to insert")
-    //     }
-    //     console.log(result.ops)
+    // db.collection('users').find({name:"Yami"}).toArray((error,users)=>{
+    //     console.log(users)
     // })
-    db.collection('tasks').insertMany([
-        {
-            description:"Buy Grocery",
-            completed: false
-        },
-        {
-            description:"Sell Laptop",
-            completed: true
-        },
-        {
-            description:"Wake Up",
-            completed:false
-        }
-    ],(error,result)=>{
-        if(error){
-            return console.log("Unable to insert new data")
-        }
-        console.log(result.ops)
+    // db.collection('users').find({name:"Yami"}).count((error,count)=>{
+    //     console.log(count)
+    // })
+    db.collection("tasks").findOne({_id:new ObjectID("612116159c209d31c0966cc3")},(error,doc)=>{
+        console.log(doc)
+    })
+    db.collection("tasks").find({completed:false}).toArray((error,docs)=>{
+        console.log(docs)
     })
 })
