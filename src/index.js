2d7db67c31cf78ea27781a449771a527e0953265
unknown <arya.wadhwani@gmail.com>
2021-08-23 01:11:49 +0530

    Added delete routes

--- src/index.js
+++ src/index.js
@@ -48,6 +48,39 @@ app.get('/users/:id',async (req,res)=>{
     }
 })
 
+// PATCH route for updating a user
+app.patch('/users/:id',async(req,res)=>{
+    const updates = Object.keys(req.body)
+    const updatesAllowed = ['name','email','password','age']
+    const isValidOperation  = updates.every((update)=>updatesAllowed.includes(update))
+    if(!isValidOperation){
+        return res.status(400).send({error:"Invalid Updates"})
+    }
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id,req.body,{ new:true, runValidators: true})
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch(e){
+        res.status(400).send(e)
+    }
+})
+
+//DELETE route for deleting a user
+app.delete('/users/:id',async(req,res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch(e){
+        res.status(500).send(e)
+    }
+})
+
 // POST route for creating new tasks and saving in the database
 app.post('/tasks',async (req,res)=>{
     const task = new Task(req.body)
@@ -86,6 +119,38 @@ app.get('/tasks/:id',async(req,res)=>{
     }
 })
 
+// PATCH route for updating a particular task
+app.patch('/tasks/:id',async (req,res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description','completed']
+    const isValidOperation = updates.every((update)=>allowedUpdates.includes(update))
+    if(!isValidOperation){
+        return res.status(400).send({error:"Invalid Update!"})
+    }
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id,req.body,{ new:true, runValidators: true})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch(e){
+        res.status(400).send()
+    }
+})
+
+// DELETE route for deleting a particular task
+app.delete('/tasks/:id',async(req,res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+
+    } catch(e){
+        res.status(500).send()
+    }
+})
 app.listen(port,()=>{
     console.log("Server is up at port ",port)
 })
