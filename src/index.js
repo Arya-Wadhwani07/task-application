dcaa36fcc7437ff7ed94b4ea8b36325ccb74e82e
unknown <arya.wadhwani@gmail.com>
2021-08-22 18:27:44 +0530

    Routes added for reading tasks and task

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-// route for creating new users and saving in the database
+// POST route for creating new users and saving in the database
 app.post('/users',(req,res)=>{
     const user = new User(req.body)
 
@@ -19,7 +19,30 @@ app.post('/users',(req,res)=>{
     })
 })
 
-// route for creating new tasks and saving in the database
+// GET route for getting all the existing users
+app.get("/users",(req,res)=>{
+    User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+// GET dynamic route for getting one user
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id
+
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+// POST route for creating new tasks and saving in the database
 app.post('/tasks',(req,res)=>{
     const task = new Task(req.body)
 
@@ -27,6 +50,29 @@ app.post('/tasks',(req,res)=>{
         res.status(201).send(task)
     }).catch((e)=>{
         res.status(400).send(e)
+    })
+})
+
+// GET route for getting all tasks
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then((tasks)=>{
+        res.send(tasks)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+// GET route for getting a paticular task
+app.get('/tasks/:id',(req,res)=>{
+    const _id = req.params.id
+
+    Task.findById(_id).then((task)=>{
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e)=>{
+        res.status(500).send()
     })
 })
 
