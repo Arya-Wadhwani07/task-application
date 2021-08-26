100a9777e80550decbabc96cab21a0e76ffd84fd
unknown <arya.wadhwani@gmail.com>
2021-08-26 23:46:55 +0530

    Added authentication on tasks and linked it with users

--- src/index.js
+++ src/index.js
@@ -26,3 +26,16 @@ app.listen(port,()=>{
     console.log("Server is up at port ",port)
 })
 
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async ()=>{
+    // const task = await Task.findById('6127ca0e2c641b05d49c6e79')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('6127c8cb38628e33c4c5d435')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+}
+main()
--- src/routers/task.js
+++ src/routers/task.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const Task = require("../models/task")
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 // POST route for creating new tasks and saving in the database
-router.post('/tasks',async (req,res)=>{
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req,res)=>{
+    const task = new Task({
+        ...req.body,
+        owner:req.user._id
+    })
 
     try{
         await task.save()
@@ -16,21 +20,24 @@ router.post('/tasks',async (req,res)=>{
 })
 
 // GET route for getting all tasks
-router.get('/tasks',async (req,res)=>{
+router.get('/tasks', auth, async (req,res)=>{
     try{
-        const tasks = await Task.find({})
-        res.send(tasks)
+        // const tasks = await Task.find({owner:req.user._id})
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch(e){
         res.status(500).send()
     }
 })
 
 // GET route for getting a paticular task
-router.get('/tasks/:id',async(req,res)=>{
+router.get('/tasks/:id',auth,async(req,res)=>{
     const _id = req.params.id
 
     try{
-        const task = await Task.findById(_id)
+        // const task = await Task.findById(_id)
+        const task = await Task.findOne({_id,owner:req.user._id})
+
         if(!task){
             return res.status(401).send()
         }
@@ -41,7 +48,7 @@ router.get('/tasks/:id',async(req,res)=>{
 })
 
 // PATCH route for updating a particular task
-router.patch('/tasks/:id',async (req,res)=>{
+router.patch('/tasks/:id', auth, async (req,res)=>{
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description','completed']
     const isValidOperation = updates.every((update)=>allowedUpdates.includes(update))
@@ -50,12 +57,13 @@ router.patch('/tasks/:id',async (req,res)=>{
     }
     try{
         // const task = await Task.findByIdAndUpdate(req.params.id,req.body,{ new:true, runValidators: true})
-        const task = await Task.findById(req.params.id)
-        updates.forEach((update)=>task[update]=req.body[update])
-        await task.save()
+        // const task = await Task.findById(req.params.id)
+        const task = await Task.findOne({_id:req.params.id,owner:req.user._id})
         if(!task){
             return res.status(404).send()
         }
+        updates.forEach((update)=>task[update]=req.body[update])
+        await task.save()
         res.send(task)
     } catch(e){
         res.status(400).send()
@@ -63,9 +71,10 @@ router.patch('/tasks/:id',async (req,res)=>{
 })
 
 // DELETE route for deleting a particular task
-router.delete('/tasks/:id',async(req,res)=>{
+router.delete('/tasks/:id', auth, async(req,res)=>{
     try{
-        const task = await Task.findByIdAndDelete(req.params.id)
+        // const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({_id:req.params.id, owner:req.user._id})
         if(!task){
             return res.status(404).send()
         }
